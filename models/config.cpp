109383db2a3d8c30acb7d6d91c321b3fad3abeba
micromartin1963 <micromartin1963@gmail.com>
2022-08-13 23:26:51 +0100

    Add files via upload

---
+++ models/config.cpp
@@ -0,0 +1,260 @@
+
+class CfgPatches
+{
+	class MyBox
+	{
+		units[] = {"newsignbase","coleby_3d_text","burton_3d_text","amble_3d_text","arnold_3d_text","balmore_mil_3d_text","belton_mil_3d_text","blyton_3d_text","bowhill_3d_text","bullcote_3d_text","burley_3d_text","claydon_3d_text","eaton_r_s_3d_text","ermine_3d_text","griston_3d_text","gunby_3d_text","harby_3d_text","horton_3d_text","huntley_prison_3d_text","lutton_3d_text","mil_cave_3d_text","morton_3d_text","saltby_3d_text","thornhill_mil_3d_text","welton_3d_text","westhead_3d_text","whisby_3d_text","woodend_3d_text","tetford_3d_text","1536_768_4"};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"DZ_Data"};
+	};
+};
+class CfgVehicles
+{
+	class HouseNoDestruct;
+
+	class 1536_768_4 : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\1536_768_4.p3d";
+	};
+
+	class tetford_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\tetford_3d_text.p3d";
+	};
+
+	class griston_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\griston_3d_text.p3d";
+	};
+
+	class woodend_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\woodend_3d_text.p3d";
+	};
+
+
+	class whisby_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\whisby_3d_text.p3d";
+	};
+
+	class westhead_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\westhead_3d_text.p3d";
+	};
+
+	class welton_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\welton_3d_text.p3d";
+	};
+
+	class thornhill_mil_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\thornhill_mil_3d_text.p3d";
+	};
+
+	class saltby_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\saltby_3d_text.p3d";
+	};
+
+	class morton_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\morton_3d_text.p3d";
+	};
+
+	class mil_cave_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\mil_cave_3d_text.p3d";
+	};
+
+	class lutton_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\lutton_3d_text.p3d";
+	};
+
+	class huntley_prison_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\huntley_prison_3d_text.p3d";
+	};
+
+
+	class horton_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\horton_3d_text.p3d";
+	};
+
+	class harby_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\harby_3d_text.p3d";
+	};
+
+	class gunby_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\gunby_3d_text.p3d";
+	};
+
+	class ermine_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\ermine_3d_text.p3d";
+	};
+
+	class eaton_r_s_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\eaton_r_s_3d_text.p3d";
+	};
+
+
+	class claydon_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\claydon_3d_text.p3d";
+	};
+
+	class burley_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\burley_3d_text.p3d";
+	};
+
+	class bullcote_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\bullcote_3d_text.p3d";
+	};
+
+	class bowhill_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\bowhill_3d_text.p3d";
+	};
+
+	class blyton_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\blyton_3d_text.p3d";
+	};
+
+	class belton_mil_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\belton_mil_3d_text.p3d";
+	};
+
+	class balmore_mil_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\balmore_mil_3d_text.p3d";
+	};
+
+	class arnold_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\arnold_3d_text.p3d";
+	};
+
+	class amble_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\amble_3d_text.p3d";
+	};
+
+	class newsignbase : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\newsignbase.p3d";
+	};
+
+	class coleby_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\coleby_3d_text.p3d";
+	};
+
+	class burton_3d_text : HouseNoDestruct
+	{
+		scope = 2;
+		displayName = "My Box";
+		descriptionShort = "My_Box";
+		model = "\fodland\models\burton_3d_text.p3d";
+	};
+
+
+};
