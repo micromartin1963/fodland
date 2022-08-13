1e20e564b6ae048303e9878c8a5beab9b558e1d0
micromartin1963 <micromartin1963@gmail.com>
2022-08-13 23:50:44 +0100

    Add files via upload

---
+++ serverstuff/core/BaseModuleInclude.c
@@ -0,0 +1,5 @@
+#include "$CurrentDir:missions\\DayZCommunityOfflineMode.fodland\\core\\ModuleManager.c"
+#include "$CurrentDir:missions\\DayZCommunityOfflineMode.fodland\\core\\StaticFunctions.c"
+
+#include "$CurrentDir:missions\\DayZCommunityOfflineMode.fodland\\core\\CommunityOfflineClient.c"
+#include "$CurrentDir:missions\\DayZCommunityOfflineMode.fodland\\core\\CommunityOfflineServer.c"
