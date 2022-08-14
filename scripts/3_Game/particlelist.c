8c9deefee7397a876d2737dccc2efaf8de36f1c2
micromartin1963 <micromartin1963@gmail.com>
2022-08-14 15:45:56 +0100

    Add files via upload

---
+++ scripts/3_Game/particlelist.c
@@ -0,0 +1,9 @@
+// Register all particles below!
+
+// Example how to register particles from a mod
+
+modded class ParticleList
+{
+	static const int MODDED_PARTICLE = RegisterParticle( "fodland/models/" , "smoke_heli_wreck_red");
+}
+
